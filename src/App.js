642b7f4f3b968864b2b8839712e2f8f92b7bf4b1
ThiagoDe <17tawi@gmail.com>
2023-04-16 17:38:07 -0700

    main backup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Footer from './componets/Footer';
 import GameBoard from './componets/GameBoard';
